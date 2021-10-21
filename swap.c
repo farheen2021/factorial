d6d99e91063a524f0d5ab0e51e06ed5014a8e32b
farheen2021 <92917383+farheen2021@users.noreply.github.com>
2021-10-21 16:14:06 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,fact=1,n;
+    printf("enter a number:");
+    scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+    printf("factorial of %d is %d found \n",n,fact);
+    return 0;
+}
